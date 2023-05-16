fb67904ee0b66bf70585cdd4371657c5c8e69f52
William Kennedy <bill@ardanlabs.com>
2023-05-16 12:42:43 -0400

    commit 16

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -47,7 +47,7 @@ func run(log *zap.SugaredLogger) error {
 			ReadTimeout     time.Duration `conf:"default:5s"`
 			WriteTimeout    time.Duration `conf:"default:10s"`
 			IdleTimeout     time.Duration `conf:"default:120s"`
-			ShutdownTimeout time.Duration `conf:"default:20s"`
+			ShutdownTimeout time.Duration `conf:"default:20s,mask"`
 			APIHost         string        `conf:"default:0.0.0.0:3000"`
 			DebugHost       string        `conf:"default:0.0.0.0:4000"`
 		}
