d6989d5564e507b5e2c156304b580c053f803324
William Kennedy <bill@ardanlabs.com>
2023-05-16 15:54:59 -0400

    commit 29

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -7,6 +7,12 @@ import (
 
 // Test is our example route.
 func Test(w http.ResponseWriter, r *http.Request) {
+
+	// Validate the data
+	// Call into the business layer
+	// Return errors
+	// Handle OK response
+
 	status := struct {
 		Status string
 	}{
