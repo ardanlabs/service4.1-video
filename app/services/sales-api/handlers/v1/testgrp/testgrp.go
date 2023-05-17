cc72eb55ad1a96944184ef82ddd7662d8960fc5d
William Kennedy <bill@ardanlabs.com>
2023-05-17 13:22:03 -0400

    commit 34

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -13,7 +13,6 @@ func Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	// Validate the data
 	// Call into the business layer
 	// Return errors
-	// Handle OK response
 
 	status := struct {
 		Status string
