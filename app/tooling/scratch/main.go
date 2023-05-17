6af4a294fb04af5b91f60c4255f5895765269a45
William Kennedy <bill@ardanlabs.com>
2023-05-17 15:26:11 -0400

    commit 42

--- app/tooling/scratch/main.go
+++ app/tooling/scratch/main.go
@@ -8,14 +8,65 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func main() {
-	err := genKey()
+	err := genToken()
+	// err := genKey()
 
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func genToken() error {
+
+	// Generating a token requires defining a set of claims. In this applications
+	// case, we only care about defining the subject and the user in question and
+	// the roles they have on the database. This token will expire in a year.
+	//
+	// iss (issuer): Issuer of the JWT
+	// sub (subject): Subject of the JWT (the user)
+	// aud (audience): Recipient for which the JWT is intended
+	// exp (expiration time): Time after which the JWT expires
+	// nbf (not before time): Time before which the JWT must not be accepted for processing
+	// iat (issued at time): Time at which the JWT was issued; can be used to determine age of the JWT
+	// jti (JWT ID): Unique identifier; can be used to prevent the JWT from being replayed (allows a token to be used only once)
+	claims := struct {
+		jwt.RegisteredClaims
+		Roles []string
+	}{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   "12345678",
+			Issuer:    "service project",
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(8760 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		},
+		Roles: []string{"ADMIN"},
+	}
+
+	method := jwt.GetSigningMethod(jwt.SigningMethodRS256.Name)
+	token := jwt.NewWithClaims(method, claims)
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return fmt.Errorf("generating key: %w", err)
+	}
+
+	str, err := token.SignedString(privateKey)
+	if err != nil {
+		return fmt.Errorf("signing token: %w", err)
+	}
+
+	fmt.Println("*********** TOKEN ************")
+	fmt.Println(str)
+	fmt.Print("\n")
+
+	return nil
+
 }
 
 func genKey() error {
