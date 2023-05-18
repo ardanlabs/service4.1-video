6687db2200ae27ecae35b5966dad0c3c303a4973
William Kennedy <bill@ardanlabs.com>
2023-05-18 12:32:27 -0400

    commit 52

--- app/tooling/scratch/main.go
+++ app/tooling/scratch/main.go
@@ -10,6 +10,7 @@ import (
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"time"
@@ -56,9 +57,20 @@ func genToken() error {
 	method := jwt.GetSigningMethod(jwt.SigningMethodRS256.Name)
 	token := jwt.NewWithClaims(method, claims)
 
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	file, err := os.Open("zarf/keys/54bb2165-71e1-41a6-af3e-7da4a0e1e2c1.pem")
 	if err != nil {
-		return fmt.Errorf("generating key: %w", err)
+		return fmt.Errorf("opening key file: %w", err)
+	}
+	defer file.Close()
+
+	pemData, err := io.ReadAll(io.LimitReader(file, 1024*1024))
+	if err != nil {
+		return fmt.Errorf("reading auth private key: %w", err)
+	}
+
+	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(pemData)
+	if err != nil {
+		return fmt.Errorf("parsing auth private key: %w", err)
 	}
 
 	str, err := token.SignedString(privateKey)
